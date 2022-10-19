5bd1ba4ffb65d62e001cf9e40d5953f46150409c
Jacob Mathew <nitrojacob@gmail.com>
2022-10-19 10:58:07 +0530

    Additional stats added. Machine setup info added to readme.

--- README.md
+++ README.md
@@ -9,6 +9,16 @@ Output is fixed width tables with result summary. Output is written to console.
 The script requires one positional argument which is the xls file generated from
 KTU-Login(app.ktu.edu.in) > Results > Semester Grade Card Report > ... > Export as XLS
 
+Setup (Ubuntu):
+You need to install python3, and python3-pip from package manager
+
+sudo apt install python3 python3-pip
+
+
+You have to install pandas and xlrd
+
+sudo pip3 install pandas xlrd
+
 Usage:
 
 ./result\_analysis.py Results.xls
--- result_analysis.py
+++ result_analysis.py
@@ -10,7 +10,7 @@ Download result xls for a semester from
 KTU-Login(app.ktu.edu.in) > Results > Semester Grade Card Report > ... > Export as XLS
 
 Provide the xls file as argument to this script.
-
+Fore more help ./result_analysis.py --help
 """
 
 import pandas as pd
@@ -61,6 +61,7 @@ class KTU2019Result(object):
     def print_pass_fail(self, codes):
         print(self.sfmt.format("Total Students:", self.student_count))
         print(self.sfmt.format("All pass:" , self.allpass_count))
+        print(self.sfmt.format("All pass Percentage:" , round(self.allpass_count * 100/self.student_count)))
         print(self.ofmt.format("Code","#fail", "#pass", "%pass"))
         for subject in self.fail_count.keys():
             if codes != None:
@@ -75,41 +76,49 @@ class KTU2019Result(object):
         self.cgpa = pd.DataFrame(data = self.df.loc[:,'Student'], columns =['Student','Score'])
         self.cgpa['Score'] = self.df.loc[:,'CGPA']
         self.cgpa.sort_values('Score',ascending=False, inplace=True, ignore_index=True)
-    def print_sgpa_cgpa(self):
+    def print_sgpa_cgpa(self, nToppers=5):
         print()
         print('SGPA toppers')
         print(self.sfmt.format('Name', 'SGPA'))
-        for i in range(5):
+        cutoff = self.sgpa.loc[nToppers-1,'Score']
+        for i in range(self.student_count):
+            if self.sgpa.loc[i,'Score'] < cutoff:
+            	break;
             ktuid,name = self.sgpa.loc[i,'Student'].split('-')
             print(self.sfmt.format(name, self.sgpa.loc[i,'Score']))
         
         print()
         print('CGPA toppers')
         print(self.sfmt.format('Name', 'CGPA'))
-        for i in range(5):
+        cutoff = self.cgpa.loc[nToppers-1,'Score']
+        for i in range(self.student_count):
+            if self.cgpa.loc[i,'Score'] < cutoff:
+            	break;
             ktuid,name = self.cgpa.loc[i,'Student'].split('-')
             print(self.sfmt.format(name, self.cgpa.loc[i,'Score']))
             
         print()
         print('SGPA Analysis')
         print(self.sfmt.format('SGPA Range', '#students'))
-        print(self.sfmt.format('SGPA==10', result.sgpa[result.sgpa.Score == 10].shape[0]))
+        print(self.sfmt.format('SGPA==10', self.sgpa[self.sgpa.Score == 10].shape[0]))
         gpa_bucket = [10, 9, 8, 7, 6, 5, 4, 0]
         for i in range(len(gpa_bucket)-1):
             ub = gpa_bucket[i]    #upper bound
             lb = gpa_bucket[i+1] #lower bound
-            print(self.sfmt.format('SGPA <'+ str(ub)+' & >=' + str(lb), result.sgpa[(result.sgpa.Score < ub) & (result.sgpa.Score >= lb)].shape[0]))
+            print(self.sfmt.format('SGPA <'+ str(ub)+' & >=' + str(lb), self.sgpa[(self.sgpa.Score < ub) & (self.sgpa.Score >= lb)].shape[0]))
+        
         
         print()
         print('CGPA Analysis')
         print(self.sfmt.format('CGPA Range', '#students'))
-        print(self.sfmt.format('CGPA==10', result.cgpa[result.cgpa.Score == 10].shape[0]))
+        print(self.sfmt.format('CGPA==10', self.cgpa[self.cgpa.Score == 10].shape[0]))
         gpa_bucket = [10, 9, 8, 7, 6, 5, 4, 0]
         for i in range(len(gpa_bucket)-1):
             ub = gpa_bucket[i]    #upper bound
             lb = gpa_bucket[i+1] #lower bound
-            print(self.sfmt.format('CGPA <'+ str(ub)+' & >=' + str(lb), result.cgpa[(result.cgpa.Score < ub) & (result.cgpa.Score >= lb)].shape[0]))
-        
+            print(self.sfmt.format('CGPA <'+ str(ub)+' & >=' + str(lb), self.cgpa[(self.cgpa.Score < ub) & (self.cgpa.Score >= lb)].shape[0]))
+        print(self.sfmt.format( 'CGPA Mean', round(self.cgpa.Score.mean(),2)))
+        print(self.sfmt.format( 'CGPA Median', self.cgpa.Score.median()))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = "Do result analysis on file obtained from KTU login")
